1cac23ebc2986543ddb710a7e8e979561b8dfe03
isaacdigs <pk07057@gmail.com>
2019-05-11 12:49:18 +0900

    Added basic components and API key from OpenWeatherMap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Titles from "./components/Titles"
+import Form from "./components/Form"
+import Weather from "./components/Weather"
+
+const API_KEY = "956f8a6aa6c080c80a159c55cbe2a0d1";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <p>My Weather App</p>
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -5,8 +5,10 @@ class Titles extends React.Component {
     return (
       <div>
         <h1>The Weather App</h1>
-        
+        <p>Find Weather, Temperature, Humidity, etc...</p>
       </div>
     )
   }
 }
+
+export default Titles;
